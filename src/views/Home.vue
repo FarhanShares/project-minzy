dcf4d768d21f40e7859706ba3a60fc614f2aa576
Farhan Israq <ishraaq21@gmail.com>
2021-01-09 02:40:30 +0600

    Added item selection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,25 @@
       <div>
         <div class="select-all-product">select-all from shop ou</div>
 
-        <template v-for="item in 10">
-          <product-item
-            :key="item"
-            class="my-3"
-            @click="handleClickingProduct"
-          />
+        <template v-for="(item, index) in shop_ou">
+          <div class="flex items-center" :key="index">
+            <div
+              v-if="item.selected"
+              class="w-2 mt-px mr-1 bg-red-500 "
+              style="height: 106px"
+            />
+            <div
+              v-if="!item.selected"
+              class="w-2 mt-px mr-1 bg-white "
+              style="height: 106px"
+            />
+            <product-item
+              :name="item.name"
+              :sku="item.sku"
+              class="my-3"
+              @click="handleClickingProduct(item, index, 'ou')"
+            />
+          </div>
         </template>
       </div>
 
@@ -54,9 +67,24 @@ export default {
     AppBreadcrumb,
     ProductItem
   },
+  data() {
+    return {
+      shop_ou: [
+        { name: "order-01", sku: "SKU001", selected: true },
+        { name: "order-02", sku: "SKU002", selected: false },
+        { name: "order-03", sku: "SKU003", selected: false },
+        { name: "order-04", sku: "SKU004", selected: false },
+        { name: "order-05", sku: "SKU005", selected: false },
+        { name: "order-06", sku: "SKU006", selected: false },
+        { name: "order-07", sku: "SKU007", selected: false }
+      ]
+    };
+  },
   methods: {
-    handleClickingProduct(name, sku) {
-      console.log(name, sku);
+    handleClickingProduct(item, index, shop) {
+      if (shop === "ou") {
+        this.shop_ou[index].selected = !this.shop_ou[index].selected;
+      }
     }
   }
 };

d9e9b026311ab0094877945cfd7ce84c491f267a
Farhan Israq <ishraaq21@gmail.com>
2021-01-08 23:51:58 +0600

    Created topbar nav link item

---
+++ src/components/topbar/TopbarNavLinkItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div @click="$emit('click')" class="flex flex-col mx-2">
+    <div class="h-4 text-sm font-normal text-right text-gray-400 lining-nums ">
+      {{ top }}
+    </div>
+
+    <div class="text-base font-medium text-gray-500">{{ title }}</div>
+
+    <div
+      class="h-4 -mt-1 text-xs font-normal text-right text-gray-400 lining-nums "
+    >
+      {{ bottom }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopbarNavLinkItem",
+  props: ["title", "top", "bottom"]
+};
+</script>
+
+<style lang="scss" scoped></style>

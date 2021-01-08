df309e216ac041caa3084915ac963ebac8583587
Farhan Israq <ishraaq21@gmail.com>
2021-01-09 02:14:34 +0600

    Display all props

--- src/components/shop/ProductItem.vue
+++ src/components/shop/ProductItem.vue
@@ -2,8 +2,21 @@
   <div class="flex items-center">
     <div class="grid grid-cols-2">
       <img :src="image" :alt="imageAlt" />
-      <div>
-        sdf
+      <div class="grid grid-cols-2 text-xs text-gray-400">
+        <div class="pl-2">
+          <div class="mt-1">{{ name }}</div>
+          <div class="mt-1">{{ model }}</div>
+          <div class="mt-1">{{ sku }}</div>
+          <div class="mt-1">type</div>
+          <div class="mt-1">{{ country }}</div>
+        </div>
+        <div class="pl-2">
+          <div class="mt-1">{{ orderingDate }}</div>
+          <div class="mt-1">{{ deliveryDate }}</div>
+          <div class="mt-1">{{ getDate }}</div>
+          <div class="mt-1">{{ type }}</div>
+          <div class="mt-1">{{ code }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -50,15 +63,20 @@ export default {
     },
     country: {
       type: [String, Number],
-      default: "green"
+      default: "USA"
     },
     code: {
       type: [String, Number],
-      default: "green"
+      default: "R61618MJ768"
     },
     hasMessage: {
       type: Boolean,
       default: false
+    }
+  },
+  computed: {
+    getDate() {
+      return "3 days left";
     }
   }
 };

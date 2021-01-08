c8276c1a25a63fb0c3d885342ba1ea08c76853c1
Farhan Israq <ishraaq21@gmail.com>
2021-01-09 02:45:20 +0600

    Selection for show gow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,8 +32,25 @@
       <div>
         <div class="select-all-product">select-all from shop gow</div>
 
-        <template v-for="item in 10">
-          <product-item :key="item" class="my-3" />
+        <template v-for="(item, index) in shop_gow">
+          <div class="flex items-center" :key="index">
+            <div
+              v-if="item.selected"
+              class="w-2 mt-px mr-1 bg-red-500 "
+              style="height: 106px"
+            />
+            <div
+              v-if="!item.selected"
+              class="w-2 mt-px mr-1 bg-white "
+              style="height: 106px"
+            />
+            <product-item
+              :name="item.name"
+              :sku="item.sku"
+              class="my-3"
+              @click="handleClickingProduct(item, index, 'gow')"
+            />
+          </div>
         </template>
       </div>
 
@@ -70,13 +87,22 @@ export default {
   data() {
     return {
       shop_ou: [
-        { name: "order-01", sku: "SKU001", selected: true },
+        { name: "order-01", sku: "SKU001", selected: false },
         { name: "order-02", sku: "SKU002", selected: false },
         { name: "order-03", sku: "SKU003", selected: false },
         { name: "order-04", sku: "SKU004", selected: false },
         { name: "order-05", sku: "SKU005", selected: false },
         { name: "order-06", sku: "SKU006", selected: false },
         { name: "order-07", sku: "SKU007", selected: false }
+      ],
+      shop_gow: [
+        { name: "name-01", sku: "SKU0001", selected: false },
+        { name: "name-02", sku: "SKU0002", selected: false },
+        { name: "name-03", sku: "SKU0003", selected: false },
+        { name: "name-04", sku: "SKU0004", selected: false },
+        { name: "name-05", sku: "SKU0005", selected: false },
+        { name: "name-06", sku: "SKU0006", selected: false },
+        { name: "name-07", sku: "SKU0007", selected: false }
       ]
     };
   },
@@ -84,6 +110,9 @@ export default {
     handleClickingProduct(item, index, shop) {
       if (shop === "ou") {
         this.shop_ou[index].selected = !this.shop_ou[index].selected;
+      }
+      if (shop === "gow") {
+        this.shop_gow[index].selected = !this.shop_gow[index].selected;
       }
     }
   }

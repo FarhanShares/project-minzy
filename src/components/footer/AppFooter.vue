34d0be695da7a8636c4e058048208d14b268c281
Farhan Israq <ishraaq21@gmail.com>
2021-01-10 17:29:52 +0600

    Created app footer

---
+++ src/components/footer/AppFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="flex items-center justify-center max-w-5xl mx-auto mt-10 mb-5 space-x-16 footer-text"
+  >
+    <div class="flex items-center">
+      <div>update folder</div>
+      <div class="ml-3 footer-badge">3</div>
+      <div>
+        <img
+          src="@/assets/icon/icon-1.png"
+          alt="icon-1"
+          class="w-10 h-12 ml-2"
+        />
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>sync online</div>
+      <img src="@/assets/icon/icon-2.png" alt="icon-2" class="w-12 h-12 ml-2" />
+    </div>
+    <div class="flex items-center">
+      fix order <input type="search" name="" id="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
+
+<style lang="scss" scoped>
+.footer-text {
+  @apply text-gray-400 text-xl font-medium;
+}
+.footer-badge {
+  @apply font-normal bg-red-400 w-4 h-4 rounded-full text-white text-sm flex items-center justify-center;
+}
+</style>

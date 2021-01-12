499e41c060c0b9c07b8e554d66ecad3ed04b1cd2
Farhan Israq <ishraaq21@gmail.com>
2021-01-13 03:39:30 +0600

    Created labels popup

---
+++ src/views/labels/LabelsPopup.vue
@@ -0,0 +1,24 @@
+<template>
+  <t-modal header="Title of the modal" name="labels-popup">
+    Content of the modal.
+    <template v-slot:footer>
+      <div class="flex justify-between">
+        <t-button type="button">
+          Cancel
+        </t-button>
+        <t-button type="button">
+          Ok
+        </t-button>
+      </div>
+    </template>
+  </t-modal>
+</template>
+
+<script>
+export default {
+  name: "LabelsPopup"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

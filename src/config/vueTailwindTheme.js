259d5ee6976e962fc99dadd26fc5cbfe71bdd1ae
Farhan Israq <ishraaq21@gmail.com>
2021-01-13 03:21:00 +0600

    Added warning variant for button

--- src/config/vueTailwindTheme.js
+++ src/config/vueTailwindTheme.js
@@ -78,12 +78,13 @@ export default {
   "t-button": {
     component: TButton,
     props: {
-      fixedClasses: "block px-4 py-2 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed",
+      fixedClasses: "block px-4 py-2 text-sm transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed",
       classes: "text-white bg-blue-500 border border-transparent shadow-sm rounded hover:bg-blue-600",
       variants: {
         secondary: "text-gray-800 bg-white border border-gray-300 shadow-sm hover:text-gray-600",
         error: "text-white bg-red-500 border border-transparent rounded shadow-sm hover:bg-red-600",
         success: "text-white bg-green-500 border border-transparent rounded shadow-sm hover:bg-green-600",
+        warning: "text-gray-700 bg-yellow-300 border border-transparent rounded shadow-sm hover:bg-yellow-200",
         link: "text-blue-500 underline hover:text-blue-600"
       }
     }

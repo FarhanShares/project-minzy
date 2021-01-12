ada8f5259445d4df930406dfbb97800f8d8d53b4
Farhan Israq <ishraaq21@gmail.com>
2021-01-12 11:42:12 +0600

    Configured axios

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import "./assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
 
+// Services
+
+// eslint-disable-next-line
+import http from "@/services/http";
+// eslint-disable-next-line
+import ou from "@/services/ou";
+// eslint-disable-next-line
+import gow from "@/services/gow";
+
 new Vue({
   router,
   store,

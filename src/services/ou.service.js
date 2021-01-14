4bc65674e45261b7f9487704a88eb694f9d7bc7b
Farhan Israq <ishraaq21@gmail.com>
2021-01-14 22:56:48 +0600

    fix: prop. name had typo

--- src/services/ou.service.js
+++ src/services/ou.service.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Axios from "axios";
 import OuConfig from "@/config/ou.config";
 
-Vue.prototype.$gow = Axios.create({
+Vue.prototype.$ou = Axios.create({
   baseURL: OuConfig.api.baseURL,
   timeout: 1000,
   headers: OuConfig.commonHeaders

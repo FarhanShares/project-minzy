888b731d8ecdb75ab3f9a26675fd9bbd8f493b64
Farhan Israq <ishraaq21@gmail.com>
2021-01-15 15:06:33 +0600

    create: spinner skeleton

---
+++ src/components/skeleton/SpinnerSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full h-full bg-white ">
+    <loading :active.sync="active" :is-full-page="false" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SpinnerSkeleton",
+  props: {
+    active: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

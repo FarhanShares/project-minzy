9fc68902dc5ec2736d352a5bbe65e39d469d8f82
Farhan Israq <ishraaq21@gmail.com>
2021-01-16 01:51:57 +0600

    add: fetch again

--- src/views/labels/LabelsPopup.vue
+++ src/views/labels/LabelsPopup.vue
@@ -1,14 +1,26 @@
 <template>
   <t-modal header="print labels" name="labels-popup" ref="popup">
     tracking codes
-    <br />
-    package name:
+    <div v-if="shouldFetchDocument" class="px-1 py-2 my-1 border-yellow-400">
+      <div class="text-blue-400">document is being generated..</div>
+
+      <div class="text-purple-600 cursor-pointer" @click="fetchDocument">
+        click to fetch again
+      </div>
+    </div>
+
+    <section v-if="pkg && docs">
+      message: {{ pkg.message }}
+      <br />
+      package id: {{ pkg.packageId }}
+    </section>
+
     <template v-slot:footer>
       <div class="flex justify-between">
         <t-button type="button" @click="$refs.popup.hide()">
           cancel
         </t-button>
-        <t-button type="button">
+        <t-button type="button" @click="handlePrint()">
           print labels
         </t-button>
       </div>
@@ -19,18 +31,44 @@
 <script>
 import emitter from "@/services/emitter.service";
 
+import { shippingDocumentJson } from "@/services/data-model.service";
+
 export default {
   name: "LabelsPopup",
   data() {
     return {
-      pkg: null,
-      docs: null
+      pkg: {},
+      docs: {
+        code: null,
+        message: null,
+        link: null
+      }
     };
   },
   mounted() {
     emitter.on("labels-data", e => {
-      console.warn(e);
+      // console.warn("s", e);
+      this.pkg = e.pkg;
+      this.docs = e.docs;
     });
+  },
+  computed: {
+    shouldFetchDocument() {
+      return this.docs && this.docs.code && this.docs.code !== 201;
+    }
+  },
+  methods: {
+    async fetchDocument() {
+      await this.$http
+        .post(
+          "/documents",
+          shippingDocumentJson(this.pkg.packageId, this.pkg.addressBarcodes[0])
+        )
+        .then(res => {
+          console.log(res.data);
+          this.docs = res.data;
+        });
+    }
   }
 };
 </script>
